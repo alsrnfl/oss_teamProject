3150bd43743079e6cd1efaa4f06eb7bf0404a643
Daniel <limms1040@gmail.com>
2024-12-13 23:12:35 +0900

    delete: handleStartSearch

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { useNavigate  } from "react-router-dom";
+// import { useNavigate  } from "react-router-dom";
 import ExerciseList from "../components/ExerciseList";
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import './HomePage.css';
 
 const HomePage = () => {
-  const navigate = useNavigate ();
-  const handleStartSearch = () => {
-    navigate('/search');
-  };
+  // const navigate = useNavigate ();
+  // const handleStartSearch = () => {
+  //   navigate('/search');
+  // };
   return (
     <div>
       <Header />
